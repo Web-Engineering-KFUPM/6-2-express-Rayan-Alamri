1d7048c2743da42cc7235cb928ce05dd910b0f6d
Rayan-Alamri <s202247900@kfupm.edu.sa>
2025-11-11 22:29:46 +0300

    Submission

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,22 +2,20 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
-const app  = express();
-const PORT = 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 
-{/*write code to define routes*/}
 app.get("/", (req, res) => {
-    res.send("Welcome to the Quote Generator API");
+  res.send("Welcome to the Quote Generator API");
 });
 
-app.get("/api/quote", (req, res) =>{
-    const quote = getRandomQuote();
-    res.json({quote})
-})
-{/*write code to create server*/}
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
